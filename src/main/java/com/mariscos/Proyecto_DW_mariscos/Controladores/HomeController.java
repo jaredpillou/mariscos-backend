121203f6987cb409134af5b8c3769351623cc2f5
Jared Tolentino <aldojarednwn@hotmail.com>
2026-02-01 19:46:37 -0600

    Despliegue final con Angular integrado

--- src/main/java/com/mariscos/Proyecto_DW_mariscos/Controladores/HomeController.java
+++ src/main/java/com/mariscos/Proyecto_DW_mariscos/Controladores/HomeController.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mariscos.Proyecto_DW_mariscos.Controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
